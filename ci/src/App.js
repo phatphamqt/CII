749c5b9885825d1c631c4e649846aeb2667f3ca0
phat.phamqt@gmail.com <phat.phamqt@gmail.com>
2020-11-10 10:03:56 +0700

    hw2

--- ci/src/App.js
+++ ci/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
 
 function App() {
+  const str = "i am healthy"
+  const newstr = str.toUpperCase() 
+  const vowel = ["U","E","O","A","I"]
+  let arr = newstr.split("")
+  for(let i=0;i<vowel.length;i++){
+    for(let j=0;j<arr.length;j++){
+      if(arr[j]==vowel[i] || arr[j]==" " ){
+        arr.splice(j,1)
+      }
+    }  
+  }
+  let sigil = []
+  for(let k=0;k<arr.length;k++){  
+    if(sigil.indexOf(arr[k])==-1){
+      sigil.push(arr[k])
+    } else {
+      let index = sigil.indexOf(arr[k])
+      sigil.splice(index,1)
+      sigil.push(arr[k])
+    }
+  }
+console.log(sigil)
+//2
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    1.  {sigil.join("")}
     </div>
   );
 }
