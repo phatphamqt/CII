3406a5e108d086073bf7e8cb322bd1565840aa5e
phat.phamqt@gmail.com <phat.phamqt@gmail.com>
2020-11-10 14:47:59 +0700

    final hw1

--- ci/src/App.js
+++ ci/src/App.js
@@ -1,129 +1,140 @@
 
 import './App.css';
 
+let str = "shopper";
+let ned = "7";
+let morseCode = "-.-. .... .- .-.. .-.. . -. --. .";
+let wide = 10;
+let height = 10;
+let sign = "$" 
 function App() {
   //TO NEAREST VOWEL
+    
 
-let str = "esbbebcdobcdddulllllall";
-let arr = str.split("");
-console.log(arr)
-let locate = []
-let vowel = ["u","e","o","a","i"]
-
-for(i=0;i<vowel.length;i++){
-    for(j=0;j<arr.length;j++){
-        if(arr[j] == vowel[i]){
-            arr[j]=0
-            locate.push(j)
-        }
-    }
-}
-
-locate.sort(function(a,b){
-    return a-b
-})
-    console.log(arr)
-    console.log(locate)
-window.reach=[]
-const reach =window.reach
-for(l=0;l<locate.length;l++){ 
-    if((locate[l])!=locate[locate.length-1]){
-        let mid = Math.round(((locate[l+1]-1)-locate[l])/2)
-        reach.push(mid)
-    }     
-}
-console.log(reach)
-if(locate[0]!=0){
-    for(i=locate[0];i>0;i--){
-        arr[i-1] = arr[i]+1
-    }
-    for(d=0;d<reach.length;d++){
-        for(a=1;a<=reach[d];a++){
-            arr[locate[d]+a]=arr[locate[d]]+a
+    function ToNearestVowel (str){
+    
+        let arr = str.split("");
+        console.log(arr)
+        
+        let locate = []
+        let vowel = ["u","e","o","a","i"]
+    
+        for(let s1=0;s1<vowel.length;s1++){
+            for(let s2=0;s2<arr.length;s2++){
+                if(arr[s2] == vowel[s1]){
+                    arr[s2]=0
+                    locate.push(s2)
+                }
+            }
         }
     
-        for(s=reach[d];s>0;s--){
-            arr[locate[d+1]-s]=arr[locate[d+1]]+s
+        locate.sort(function(a,b){
+            return a-b
+        })
+        console.log(arr)
+        console.log(locate)
+        window.reach=[]
+        const reach =window.reach
+        for(let s3=0;s3<locate.length;s3++){ 
+            if((locate[s3])!=locate[locate.length-1]){
+                let mid = Math.round(((locate[s3+1]-1)-locate[s3])/2)
+                reach.push(mid)
+            }     
         }
+        console.log(reach)
+        if(locate[0]!=0){
+            for(let s4=locate[0];s4>0;s4--){
+                arr[s4-1] = arr[s4]+1
+            }
+            for(let s5=0;s5<reach.length;s5++){
+                for(let s6=1;s6<=reach[s5];s6++){
+                    arr[locate[s5]+s6]=arr[locate[s5]]+s6
+                }
+        
+                for(let s7=reach[s5];s7>0;s7--){
+                    arr[locate[s5+1]-s7]=arr[locate[s5+1]]+s7
+                }
+            }
+            for(let s8=locate[locate.length-1];s8<arr.length;s8++){
+                if(arr[s8]!=arr[arr.length-1]){
+                    arr[s8+1] = arr[s8] + 1 
+                } 
+            }
+            console.log(arr)
+            } else {
+                for(let s9=0;s9<reach.length;s9++){
+                    for(let s10=1;s10<=reach[s9];s10++){
+                        arr[locate[s9]+s10]=arr[locate[s9]]+s10
+                    }
+        
+                    for(let s11=reach[s9];s11>0;s11--){
+                        arr[locate[s9+1]-s11]=arr[locate[s9+1]]+s11
+                    }
+                }
+                for(let s12=locate[locate.length-1];s12<arr.length;s12++){
+                    if(arr[s12]!=arr[arr.length-1]){
+                        arr[s12+1] = arr[s12] + 1 
+                    } 
+                }
+                console.log(arr)
+            } 
+        return arr.join(",")
     }
-    for(c=locate[locate.length-1];c<arr.length;c++){
-        if(arr[c]!=arr[arr.length-1]){
-            arr[c+1] = arr[c] + 1 
-        } 
-    }
-    console.log(arr)
-} else {
-    for(d=0;d<reach.length;d++){
-        for(a=1;a<=reach[d];a++){
-            arr[locate[d]+a]=arr[locate[d]]+a
-        }
     
-        for(s=reach[d];s>0;s--){
-            arr[locate[d+1]-s]=arr[locate[d+1]]+s
-        }
-    }
-    for(c=locate[locate.length-1];c<arr.length;c++){
-        if(arr[c]!=arr[arr.length-1]){
-            arr[c+1] = arr[c] + 1 
-        } 
-    }
-    console.log(arr)
-} 
-
 
 // JUNCTION OR SELF
-
-let num =Number(7)
-let juncnumber = window.juncnumber = []
-let arr =[]
-for(let i=0;i<num;i++){
-    let str=`${i}`
-    arr=str.split("")
-    let sum=0
-    for(j=0;j<arr.length;j++){
-       sum += Number(arr[j])
+function junctionOrSelf(ned){
+    let num =Number(ned)
+    let juncnumber = window.juncnumber = []
+    let ar =[]
+    for(let j1=0;j1<num;j1++){
+        let str=`${j1}`
+        ar=str.split("")
+        let sum=0
+        for(let j2=0;j2<ar.length;j2++){
+           sum += Number(ar[j2])
+        }
+        if((j1+sum)==num){
+            console.log(j1)
+            juncnumber.push(j1)
+        }
     }
-    if((i+sum)==num){
-        console.log(i)
-        juncnumber.push(i)
+    if(juncnumber.length>0){
+        console.log(juncnumber + '\n' 
+       + num + " is a juncnumber")
+       return (num + " is a juncnumber")
+    } else {
+        console.log(num + " is a selfnumber")
+        return (num + " is a selfnumber")
     }
 }
-if(juncnumber.length>0){
-    console.log(juncnumber + '\n' 
-   + num + " is a juncnumber")
-} else {
-    console.log(num + " is a selfnumber")
+
+
+// MORSE CODE DECODED
+
+function morseDecoded (){
+    const morseToDots = {
+        ".-":"A", "-...":"B", "-.-.":"C", "-..":"D", ".":"E", "..-.":"F",
+        "--.":"G", "....":"H", "..":"I", ".---":"J", "-.-":"K", ".-..":"L",
+        "--":"M", "-.":"N", "---":"O", ".--.":"P", "--.-":"Q", ".-.":"R",
+        "...":"S", "-":"T", "..-":"U", "...-":"V", ".--":"W", "-..-":"X",
+        "-.--":"Y", "--..":"Z", "-----":"0", ".----":"1", "..---":"2",
+        "...--":"3", "....-":"4", ".....":"5", "-....":"6", "--...":"7",
+        "---..":"8", "----.":"9", "-.-.--":"!", "   ":" ", "..--..":"?",
+        ".-.-.-":".", ".----.":"\"", "---..." :":", "--..--":", ", " ":""
+      }
+      
+      
+      let morseCodearr= morseCode.split(" ")
+      console.log(morseCodearr)
+      let decoded=[]
+      for(let m1=0;m1<morseCodearr.length;m1++){
+          decoded.push(morseToDots[morseCodearr[m1]])
+      }
+      console.log(decoded.join(""))
+      return decoded.join("")
 }
 
-MORSE CODE DECODED
-
-decodeMorse(".... . .-.. .--.   -- .   -.-.--") ➞ "HELP ME !"
-
-
-decodeMorse("-.-. .... .- .-.. .-.. . -. --. .") ➞ "CHALLENGE"
-
-
-const morseCode = "-.-. .... .- .-.. .-.. . -. --. ."
-
-const morseToDots = {
-  ".-":"A", "-...":"B", "-.-.":"C", "-..":"D", ".":"E", "..-.":"F",
-  "--.":"G", "....":"H", "..":"I", ".---":"J", "-.-":"K", ".-..":"L",
-  "--":"M", "-.":"N", "---":"O", ".--.":"P", "--.-":"Q", ".-.":"R",
-  "...":"S", "-":"T", "..-":"U", "...-":"V", ".--":"W", "-..-":"X",
-  "-.--":"Y", "--..":"Z", "-----":"0", ".----":"1", "..---":"2",
-  "...--":"3", "....-":"4", ".....":"5", "-....":"6", "--...":"7",
-  "---..":"8", "----.":"9", "-.-.--":"!", "   ":" ", "..--..":"?",
-  ".-.-.-":".", ".----.":"\"", "---..." :":", "--..--":", ", " ":""
-}
-
-
-let morseCodearr= morseCode.split(" ")
-console.log(morseCodearr)
-let decoded=[]
-for(i=0;i<morseCodearr.length;i++){
-    decoded.push(morseToDots[morseCodearr[i]])
-}
-console.log(decoded.join(""))
 
 
 // PICTURE FRAME
@@ -133,13 +144,13 @@ function dirtyFrame(a,b,c) {
     const frame = [a,b,c]
     let line1 = [""]
     let line2 = [""]
-    for(i=0;i<frame[0];i++){
+    for(let f1=0;f1<frame[0];f1++){
         line1[0] +=frame[2]
     }
     
     
     line2[0] += frame[2]
-    for(k=0;k<(frame[0]-2);k++){
+    for(let f2=0;f2<(frame[0]-2);f2++){
         line2[0] += " "
     }
     line2[0] += frame[2]
@@ -151,25 +162,30 @@ function dirtyFrame(a,b,c) {
     let iKnowItsDirty = []
     
     iKnowItsDirty.push(line1)
-    for(d=0;d<frame[1]-2;d++){
+    for(let f3=0;f3<frame[1]-2;f3++){
         iKnowItsDirty.push(line2)
     }
     iKnowItsDirty.push(line1)
     
     console.log(iKnowItsDirty)
+    return iKnowItsDirty.join("")
 }
 
 
-dirtyFrame(19,19,"$")
-
   return (
     <div>
-      1. TO NEAREST VOWEL
-      
-      2.
-      3.
-      4.
-      5.
+      1. TO NEAREST VOWEL <br></br>
+        String: {str} <br></br>
+        => {ToNearestVowel(str)} <br></br><br></br>
+      2. JUNCTION OR SELF NUMBER<br></br>
+        Number given: {ned}  <br></br>
+        => {junctionOrSelf(ned)} <br></br>
+      3.MORSE CODE DECODED <br></br>
+        Code given: {morseCode} <br></br>
+        => {morseDecoded()} <br></br> 
+      4.PICTURE FRAME <br></br>
+        Wide: {wide} Height: {height} Sign: {sign} <br></br>
+        => {dirtyFrame(wide,height,sign)}
     </div>
   );
 }
